7a9cbb7d2c74519a2d137a63eda056935bf2603c
intoblack <intoblack86@gmail.com>
2014-10-24 20:47:20 +0800

    提交主题  继续写末班

--- mortred/views.py
+++ mortred/views.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from django.template import Template , Context
 from django.http import HttpResponse
 from django.template.loader import get_template
@@ -9,18 +10,22 @@ from django.conf import settings
 # from django.core.management import setup_environ
 
 
+class Article(object):
+
+    def __init__(self):
+        self.title = '标题'
+        self.summary = '摘要'
+        self.ori_url = 'http://www.baidu.com'
+
+
 def show_index(titles):
     t = get_template('index.html')
-    html = t.render(Context({'titles' : [ 12 ,2 , 3]}))
+    html = t.render(Context({'articles' : [Article()] }))
     return HttpResponse(html)
 
 
 
 
-if __name__ == '__main__':
-    django.setup()
-    # settings.configure()
-    show_index([])
 
 
 
