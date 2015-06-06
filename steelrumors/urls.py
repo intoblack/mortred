2b90ea38f1690e30265bcdf64bed561b0a24ad10
intoblack <intoblack86@gmail.com>
2015-06-06 20:14:05 +0800

    添加别人的django 到工程内部 ， 基于这个基础上去修改

--- steelrumors/urls.py
+++ steelrumors/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
+
 from django.contrib import admin
+admin.autodiscover()
+
+from links.views import LinkListView
 
 urlpatterns = patterns('',
-    url(r'^$', 'mortred.views.show_index'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', LinkListView.as_view(), name='home')
 )
